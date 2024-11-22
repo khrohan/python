86e2ff09506d385f4843d548c91a4aa593150e92
Kh Tabib Rohani Hasan <33065111+khrohan@users.noreply.github.com>
2024-11-22 22:58:17 +0600

    Delete saveallbooks.py

--- saveallbooks.py
+++
@@ -1,5 +0,0 @@
-def saveallbooks(all_books):
-    with open("all_books.csv", "w") as fp:
-        for book in all_books:
-            line = f"{book['title']}, {book['author']}, {book['isbn']}, {book['year']}, {book['price']}, {book['quantity']}\n"
-            fp.write(line)
